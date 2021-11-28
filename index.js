52b603ffcbca546f5815607570239879b030523d
Sandeepkumar222 <83975564+Sandeepkumar222@users.noreply.github.com>
2021-11-28 13:00:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ async function AppServer() {
     });
 
     //Routes
-    app.use("/forgotPassword", forgotPasswordRoute);
+    app.get("/forgotPassword", forgotPasswordRoute);
     app.use("/auth", authRoute);
 
     //Checking token
